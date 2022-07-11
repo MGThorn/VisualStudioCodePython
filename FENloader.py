104e2d3c4d90aab9a26bd1da8eead39c75dad923
MGThornHomePC <Lennart.Dorn1@outlook.de>
2022-07-11 23:02:49 +0200

    FENloader added

---
+++ FENloader.py
@@ -0,0 +1,19 @@
+translatorDikt = {
+    "p":"Pawn",
+    "n":"Knight",
+    "b":"Bishop",
+    "r":"Rook",
+    "q":"Queen",
+    "k":"King",
+
+}
+FenString ="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+i = 0
+
+
+while i<len(FenString):
+    print("test")
+    
+    i+=1
+print(len(FenString))
+    
